d15adf53fcc5108739be5767c02b96024cd9303c
Dale Corns <drcorns@hotmail.com>
2015-01-05 12:24:58 -0800

    add request.js

--- client.js
+++ client.js
@@ -6,18 +6,33 @@
 var net = require('net');
 module.exports = function(clnt){
   this.clnt = clnt;
-  var client;
-  this.connect = function(clntport){
-    client = net.connect({port: clntport}, function(){
+  var client = 0;
+  var prt;
+  this.connect = function cnct(clntport, retry){
+    prt = clntport;
+    retry = retry | 0;
+    client = net.connect({port: clntport}, function(err){
+      if(err) return cb(err, null);
       console.log(clnt + 'connected to port ' + clntport);
       client.write(clnt + 'connected');
     });
     client.on('data', function(data){
       console.log(data.toString());
-      client.end();
     });
     client.on('end', function(){
       console.log('Disconnected from port ' + clntport);
-    })
-  }
+    });
+    client.on('error', function(err){
+      if(err.code === 'ECONNREFUSED' && retry > 0){
+        console.log('Connection refused on port ' + prt + ', but will try ' + retry + ' times.');
+        retry--;
+        cnct(prt,  retry);
+      }
+      else {
+        console.dir(err);
+        client.end();
+      }
+    });
+    return client;
+  };
 };
---
+++ drcRequest.js
@@ -0,0 +1,13 @@
+/**
+ * drcRequest
+ * Created by dcorns on 1/5/15.
+ */
+'use strict';
+var Client = require('./client');
+
+var firstClient = new Client('firstClient');
+firstClient.connect(3000, 10);
+
+var secondClient = new Client('secondClient');
+secondClient.connect(3050, 5);
+
